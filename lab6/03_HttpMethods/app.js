3575cd6ee1837efaa6cbee86c4658cdb1b5c1efb
Dawid Macek <andrzej@localhost.localdomain>
2020-01-12 16:22:23 +0100

    Finish lab6

--- lab6/03_HttpMethods/app.js
+++ lab6/03_HttpMethods/app.js
@@ -28,16 +28,63 @@ app.get("/item", function(request, response) {
 });
 
 /* PUT /item/:name -- add (put) new item to the collection */
-app.put("/item/:name", function(request, response) {
-  printReqSummary(request);
-  const itemName = request.params.name;
-  /* Is the item in collection? */
-  if (items.includes(itemName)) {
-    response.send(`<p>Item "${itemName}" already in collection</p>`);
-  } else {
-    items.push(itemName);
-    response.send(`<p>Item "${itemName}" added successfully</p>`);
-  }
+// app.put("/item/:name", function(request, response) {
+//   printReqSummary(request);
+//   const itemName = request.params.name;
+//   /* Is the item in collection? */
+//   if (items.includes(itemName)) {
+//     response.send(`<p>Item "${itemName}" already in collection</p>`);
+//   } else {
+//     items.push(itemName);
+//     response.send(`<p>Item "${itemName}" added successfully</p>`);
+//  }
+// });
+
+/* PUT /item/:old_name -- updates item with old_name with name */
+app.put("/item/:old_name", function(request, response) {
+    printReqSummary(request);
+
+    let oldName = request.params.old_name,
+        newName = request.query.name,
+        itemIndex = items.indexOf(oldName);
+
+    if(newName === null || newName === undefined) {
+        response.send("<p>Missing 'name' parameter.</p>");
+        return;
+    }
+
+    if(itemIndex === -1) {
+        response.send(`<p>Item "${oldName}" doesn't exists.</p>`);
+        return;
+    }
+
+    if(items.includes(newName)) {
+        response.send(`<p>Name "${newName}" is taken.</p>`);
+        return;
+    }
+
+    items[itemIndex] = newName;
+    response.send(`<p>Item "${oldName}" changed name to "${newName}."</p>`);
+});
+
+/* POST /item -- add new item to the collection */
+app.post("/item", function(request, response) {
+    printReqSummary(request);
+
+    let itemName = request.query.name;
+
+    if(itemName === null || itemName === undefined) {
+        response.send("<p>Missing 'name' parameter.</p>");
+        return;
+    }
+
+    /* Is the item in collection? */
+    if (items.includes(itemName)) {
+        response.send(`<p>Item "${itemName}" already in collection</p>`);
+    } else {
+        items.push(itemName);
+        response.send(`<p>Item "${itemName}" added successfully</p>`);
+    }
 });
 
 /* DELETE /item/:name -- remove a given item from the collection */
